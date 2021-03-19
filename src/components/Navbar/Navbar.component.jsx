2ffffcb9c07bb14e57736b9a3996af67264929ab
Nelly Ramirez <nella.rg25@gmail.com>
2021-03-19 14:04:49 -0600

    feature navbar

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -6,11 +6,14 @@ const NavHeader = styled.nav`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  /* justify-content: flex-start; */
+  justify-content: flex-start;
+  color: #fff;
   padding: 1rem;
-  /* background-color: #2D3047; */
   background-color: #1b998b;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  span {
+    padding: 0.25rem;
+  }
 `;
 const NavToggle = styled.button`
   background-color: transparent;
@@ -25,8 +28,38 @@ const NavTitle = styled.p`
   font-style: italic;
   font-weight: 300;
   vertical-align: middle;
-  color: white;
+  color: #fff;
   margin: 0.5rem 0;
+`;
+const NavContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  @media (max-width: 360px) {
+    margin-left: 1rem;
+  }
+`;
+const Switch = styled.label`
+  display: flex;
+  border-radius: 9999px;
+  height: 22px;
+  width: 50px;
+  background-color: rgba(255, 255, 255, 0.15);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  input {
+    display: none;
+  }
+`;
+
+const Circle = styled.div`
+  border-radius: 9999px;
+  width: 22px;
+  background-color: #fff;
+`;
+const Avatar = styled.img`
+  max-height: 40px;
+  width: auto;
+  margin: 0.5rem 0 0.5rem 1rem;
 `;
 
 function NavBar() {
@@ -37,6 +70,14 @@ function NavBar() {
       </NavToggle>
       <NavTitle>Wizeline Project</NavTitle>
       <SearchInput />
+      <NavContainer>
+        <Switch>
+          <input type="checkbox" />
+          <Circle />
+        </Switch>
+        <span>Dark Mode</span>
+        <Avatar src="./assets/avatar.png" alt="avatar" />
+      </NavContainer>
     </NavHeader>
   );
 }
